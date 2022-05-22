cfa172271a3df6cf0fe4cd9ca7bd27ccf7b4f31e
mustafaahmedani <mustafa_ahmedani@outlook.com>
2022-05-21 23:25:06 -0700

    initial build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './utils/navbar/navbar.component';
+import { FooterComponent } from './utils/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CvComponent } from './pages/cv/cv.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    CvComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
